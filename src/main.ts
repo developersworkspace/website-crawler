9a4384f0aa04425f5425fbe11e1220805fb53e9a
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-08-25 21:20:12 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { Crawler } from './crawler';
 import * as fs from 'fs';
 import { remote } from 'electron';
 
+const loader = document.querySelector('div.loading') as HTMLDivElement;
+
 const txtListOfURLs = document.querySelector('textarea#listOfURLs') as HTMLTextAreaElement;
 
 const txtListOfPatterns = document.querySelector('textarea#listOfPatterns') as HTMLTextAreaElement;
@@ -11,6 +13,8 @@ const txtDepth = document.querySelector('input#depth') as HTMLInputElement;
 const btnCrawl = document.querySelector('button#crawl');
 
 btnCrawl.addEventListener('click', async () => {
+  loader.style.display = 'block';
+
   const urls: string[] = txtListOfURLs.value.split('\n');
   const patterns: string[] = txtListOfPatterns.value.split('\n');
 
@@ -37,7 +41,18 @@ btnCrawl.addEventListener('click', async () => {
     );
   }
 
-  const filePath: string = remote.dialog.showSaveDialog(undefined);
+  const filePath: string = remote.dialog.showSaveDialog(undefined, {
+    filters: [
+      {
+        name: 'CSV',
+        extensions: ['*.csv'],
+      },
+    ],
+  });
 
-  fs.writeFileSync(filePath, state.join('\n'));
+  if (filePath) {
+    fs.writeFileSync(filePath, state.join('\n'));
+  }
+
+  loader.style.display = 'none';
 });
